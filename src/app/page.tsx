7fe5c0e8e279068dee6776ce2ece1d48ec8b5dc3
Sébastien PERIN <seb.trash@gmail.com>
2024-08-06 11:21:03 +0200

    issue: using bson from client component page produces "Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+// Comment "use client" to get rid of the following error:
+/*
+Error: Element type is invalid: expected a string (for built-in components)
+or a class/function (for composite components) but got: undefined. You likely
+forgot to export your component from the file it's defined in, or you might have
+mixed up default and named imports.
+
+Check the render method of `ClientPageRoot`.
+*/
+"use client";
+
 import Image from "next/image";
+import { BSON } from "bson";
 
 export default function Home() {
+  const serializedPayload = BSON.serialize({ a: 1 });
+  const payload = BSON.deserialize(serializedPayload);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          BSON payload test:&nbsp;
+          <code className="font-mono font-bold">{payload.a}</code>
+        </p>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
